cb8c190e47de72e005797dc21cf7820dbb95d7bc
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-02 13:33:00 -0400

    Time: 94 ms (69.79%), Space: 14.4 MB (44.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        visited = {}
+        visited = {}  # Initialize a dictionary to act as a python hash map
         
-        for i, value in enumerate(nums):
-            toFind = target - value
+        for i, value in enumerate(nums):  # Enumerate goes through list index and values respectively
+            toFind = target - value  # This does the math to figure out what number needs to be found still
             
-            if toFind in visited:
-                return [i, visited[toFind]]
-            else:
-                visited[value] = i
+            if toFind in visited:  # If the value from above is found as a key in the 'hash map', then...
+                return [i, visited[toFind]]  # Return the index of current value and the index of the found value
+            else:  # Otherwise, ...
+                visited[value] = i  # Add the current value to the 'hash map'
