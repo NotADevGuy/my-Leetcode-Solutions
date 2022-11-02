51bc4f61e3075e4653e741c3a3f71a49889b3df9
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-02 13:30:49 -0400

    Time: 73 ms (85.68%), Space: 14.4 MB (44.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # for i, x in enumerate(nums):
-        #     print("i: {} x: {}".format(i, x))
-        for i in range(len(nums)):
-            for x in range(i + 1, len(nums)):
-                if (nums[i] + nums[x] == target) and (i != x):
-                    return [i, x]
+        visited = {}
+        
+        for i, value in enumerate(nums):
+            toFind = target - value
+            
+            if toFind in visited:
+                return [i, visited[toFind]]
+            else:
+                visited[value] = i
