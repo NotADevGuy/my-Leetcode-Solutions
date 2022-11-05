64f9c6d0d809f83a9e86691f76001fddf83c72a0
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-04 22:02:21 -0400

    Time: 169 ms (40.90%), Space: 13.7 MB (23.55%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -7,24 +7,29 @@ class Solution(object):
         """
         length = (len(nums1) + len(nums2))
         srt = []
-        # i1 = i2 = 0
-        # while i1 != len(nums1) or i2 != len(nums2):
-        #     if i1 == len(nums1):
-        #         srt.append(nums2[i2])
-        #         i2 += 1
-        #     elif i2 == len(nums2):
-        #         srt.append(nums1[i1])
-        #         i1 += 1
-        #     else:
-        #         if nums1[i1] < nums2[i2]:
-        #             srt.append(nums1[i1])
-        #             i1 += 1
-        #         else:
-        #             srt.append(nums2[i2])
-        #             i2 += 1
         srt = (nums1 + nums2)
         srt.sort()
         if length % 2 == 1:
             return srt[length//2]
         else:
             return float(srt[length//2] + srt[(length//2) - 1]) / 2
+        
+        # My sorting, since it doesn't make it faster???
+        """
+        i1 = i2 = 0
+        while i1 != len(nums1) or i2 != len(nums2):
+            if i1 == len(nums1):
+                srt.append(nums2[i2])
+                i2 += 1
+            elif i2 == len(nums2):
+                srt.append(nums1[i1])
+                i1 += 1
+            else:
+                if nums1[i1] < nums2[i2]:
+                    srt.append(nums1[i1])
+                    i1 += 1
+                else:
+                    srt.append(nums2[i2])
+                    i2 += 1
+        """
+        
