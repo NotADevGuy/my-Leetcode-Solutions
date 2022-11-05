23d5c29a7e155efb290eb1662d7f09844ca68507
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-04 21:12:09 -0400

    Time: 38 ms (97.08%), Space: 13.6 MB (85.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,24 +7,19 @@ class Solution(object):
         seen = {}
         start = 0
         maxlength = 0
+        for i, char in enumerate(s):
+            if char in seen and start <= seen[char]:
+                start = seen[char] + 1
+            else:
+                maxlength = max(maxlength, (i - start + 1))
+            seen[char] = i
+        return maxlength
+    
+        """
         for i in range(len(s)):
             if s[i] in seen and start <= seen[s[i]]:
                 start = seen[s[i]] + 1
             else:
                 maxlength = max(maxlength, (i - start + 1))
             seen[s[i]] = i
-        return maxlength        
-        
-        
-        """
-        used = {}
-        max_length = start = 0
-        for i, c in enumerate(s):
-            if c in used and start <= used[c]:
-                start = used[c] + 1
-            else:
-                max_length = max(max_length, i - start + 1)
-
-            used[c] = i
-        return max_length    
-        """
+        return maxlength"""
