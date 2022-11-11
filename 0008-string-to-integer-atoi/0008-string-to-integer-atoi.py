102e8e75ea2bc11fa459c568ab271d063bc6fa8d
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-11 13:34:28 -0500

    Time: 43 ms (52.78%), Space: 13.4 MB (76.63%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        neg = False
+        nums = []
+        s = s.lstrip()
+        tmp = ''
+        if len(s) == 0:
+            return 0
+        if s[0] == '-':# or s[0] == '+':
+            s = s[1:]
+            neg = True
+        elif s[0] == '+':
+            s = s[1:]
+        for letter in s:
+            if letter.isdigit():
+                tmp += letter
+            elif not letter.isdigit():
+                break
+
+        try:
+            if neg:
+                tmp = int('-' + tmp)
+                if tmp < (-2**31):
+                    tmp = -2**31
+            else:
+                tmp = int(tmp)
+                if tmp > (2**31 - 1):
+                    tmp = (2**31 - 1)
+        except:
+            return 0
+
+        return tmp
