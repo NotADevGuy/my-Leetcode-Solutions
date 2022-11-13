d4af2993eee9ae6f56e5615352ed964e8f103ac6
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-13 14:44:30 -0500

    Time: 92 ms (72.32%), Space: 13.5 MB (11.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,36 +1,13 @@
 class Solution(object):
-    def myAtoi(self, s):
+    def isPalindrome(self, x):
         """
-        :type s: str
-        :rtype: int
+        :type x: int
+        :rtype: bool
         """
-        neg = False
-        nums = []
-        s = s.lstrip()
-        tmp = ''
-        if len(s) == 0:
-            return 0
-        if s[0] == '-':# or s[0] == '+':
-            s = s[1:]
-            neg = True
-        elif s[0] == '+':
-            s = s[1:]
-        for letter in s:
-            if letter.isdigit():
-                tmp += letter
-            elif not letter.isdigit():
-                break
-
-        try:
-            if neg:
-                tmp = int('-' + tmp)
-                if tmp < (-2**31):
-                    tmp = -2**31
-            else:
-                tmp = int(tmp)
-                if tmp > (2**31 - 1):
-                    tmp = (2**31 - 1)
-        except:
-            return 0
-
-        return tmp
+        if x < 0:
+            return False
+        x = str(x)
+        if x[::-1] == x:
+            return True
+        else:
+            return False
