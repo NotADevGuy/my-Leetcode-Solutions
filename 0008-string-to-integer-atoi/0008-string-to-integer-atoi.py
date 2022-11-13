f2ffe1f8117386b5ae1dfb8cb0feb742f35b86ba
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-13 14:38:05 -0500

    Time: 56 ms (16.65%), Space: 13.6 MB (51.78%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -4,9 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        err = False
+        tmp = ""
+        s = s.lstrip()
+        for item in s:
+            if item.isdigit():
+                tmp += item
+                err = True
+            elif err == True:
+                break
+            elif item == '-':
+                tmp += '-'
+            elif item == '+':
+                tmp += '+'
+            else:
+                break
+        try:
+            tmp = int(tmp)
+        except:
+            return 0
+        
+        if tmp < (-2**31):
+            return -2**31
+        elif tmp > (2**31 - 1):
+            return 2**31 - 1
+        else:
+            return tmp
+        
+        """
         neg = False
         s = s.lstrip()
         tmp = ''
+        
         if len(s) == 0:
             return 0
         if s[0] == '-':
@@ -26,40 +55,6 @@ class Solution(object):
                 return -2**31
             elif tmp > (2**31 - 1):
                     return 2**31 - 1
-        except:
-            return 0
-
-        return tmp
-
-        
-        
-        """
-        neg = False
-        nums = []
-        s = s.lstrip()
-        tmp = ''
-        if len(s) == 0:
-            return 0
-        if s[0] == '-':# or s[0] == '+':
-            s = s[1:]
-            neg = True
-        elif s[0] == '+':
-            s = s[1:]
-        for letter in s:
-            if letter.isdigit():
-                tmp += letter
-            elif not letter.isdigit():
-                break
-
-        try:
-            if neg:
-                tmp = int('-' + tmp)
-                if tmp < (-2**31):
-                    tmp = -2**31
-            else:
-                tmp = int(tmp)
-                if tmp > (2**31 - 1):
-                    tmp = (2**31 - 1)
         except:
             return 0
 
