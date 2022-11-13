34183e4868ced0e046286a89db1bc10ae701d604
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-11-13 14:39:28 -0500

    Time: 44 ms (49.52%), Space: 13.6 MB (22.57%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -7,18 +7,18 @@ class Solution(object):
         err = False
         tmp = ""
         s = s.lstrip()
+        
         for item in s:
             if item.isdigit():
                 tmp += item
                 err = True
             elif err == True:
                 break
-            elif item == '-':
-                tmp += '-'
-            elif item == '+':
-                tmp += '+'
+            elif item in ('-', '+'):
+                tmp += item
             else:
                 break
+        
         try:
             tmp = int(tmp)
         except:
@@ -30,33 +30,3 @@ class Solution(object):
             return 2**31 - 1
         else:
             return tmp
-        
-        """
-        neg = False
-        s = s.lstrip()
-        tmp = ''
-        
-        if len(s) == 0:
-            return 0
-        if s[0] == '-':
-            s = s[1:]
-            neg = True
-        elif s[0] == '+':
-            s = s[1:]
-        for letter in s:
-            if letter.isdigit():
-                tmp += letter
-            else:
-                break
-
-        try:
-            tmp = int('-' + tmp) if neg else int(tmp)
-            if tmp < (-2**31):
-                return -2**31
-            elif tmp > (2**31 - 1):
-                    return 2**31 - 1
-        except:
-            return 0
-
-        return tmp
-        """
